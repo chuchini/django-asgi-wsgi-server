30dacf115bdffb964506a2156877add1916fd0e5
Ubuntu <ubuntu@ip-172-31-33-55.us-west-2.compute.internal>
2021-04-02 00:48:53 +0000

    Carga inicial

---
+++ async_test/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for async_test project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+import django
+#from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'async_test.settings')
+
+django.setup()
+
+#application = get_asgi_application()
+application = get_default_application()
